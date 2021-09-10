178cc9ffa676371700bd5cb42f94c0483268ca7c
Johan02 <johandif3@gmail.com>
2021-09-11 00:14:44 +0200

    Basics

---
+++ Framework.lua
@@ -0,0 +1,19 @@
+local Aircraft = script.Parent.Parent
+
+local CodeFolder = script.Parent
+local BodyKit = Aircraft.BodyKit
+
+local CriticalStructure = BodyKit.CriticalStructure
+
+
+
+for indexPart, Part in pairs(BodyKit:GetDescendants()) do
+    if Part:IsA('BasePart') then
+        
+
+
+
+
+
+    end
+end
